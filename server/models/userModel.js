5546959d4375402e6e8c701178616e202d854f17
Sebastian Sarmiento <sebastiansarmiento@Sebastians-Air.attlocal.net>
2023-07-02 19:01:21 -0400

    added bcrypt functionality to userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs')
+
+userSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, SALT_WORK_FACTOR,
+  (err, has) => {
+    if (err) return next(err);
+    this.password = hash;
+    return next();
+  })
+});
+
 // set a schema for the 'users' collectionx
 const userSchema = new Schema({
   first_name: {
