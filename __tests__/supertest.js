8b942d0e53dcbac32358bef47baa6d752e2664bf
Rodrigo Samour Calderon <rodrigosamourcalderon@Rodrigos-MacBook-Pro.local>
2023-07-03 19:30:52 -0400

    added initial structure for testing suite and some addtnl webpack config for testing

---
+++ __tests__/supertest.js
@@ -0,0 +1,9 @@
+// declare all packages to be imported
+const request = require('supertest');
+
+// initialize our server for testing
+const server = 'http://localhost:3000';
+
+describe('Route integration', () => {
+  describe('/'), () => {};
+});
